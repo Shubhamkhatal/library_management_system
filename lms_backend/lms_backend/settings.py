d63bd8d193c38b3001bf460add0019c442838773
Shubhamkhatal <66289052+Shubhamkhatal@users.noreply.github.com>
2021-10-31 09:03:44 +0530

    Update settings.py

--- lms_backend/lms_backend/settings.py
+++ lms_backend/lms_backend/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jm4(u!_u4#jd6l)kwwx4!ju1+vp1(7mw9$fx52do)(7v_fc^*k'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
